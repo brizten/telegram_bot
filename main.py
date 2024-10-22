a2a178966d65c16459b639c158a6499e142bb85d
unknown <Kuanysh>
2024-10-23 02:15:45 +0600

    new_commands

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import asyncio
 import logging
 import sys
-
 import keyring
-
 from asteroid import detector
 from weather import get_weather
-
 from aiocron import crontab
-from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 
 TOKEN = keyring.get_password('telega', 'api')
 CHAT_ID = keyring.get_password('telega', 'chat_id')
-
 
 dp = Dispatcher()
 
@@ -36,18 +33,40 @@ async def send_asteroid_info(bot: Bot) -> None:
     asteroid_info = detector()
     await bot.send_message(chat_id=CHAT_ID, text=asteroid_info)
 
-
-async def weather(bot: Bot) -> None:
+@dp.message(Command("asteroid"))
+async def send_asteroid_info(message: Message) -> None:
     """
-    send info about weather
+    Sends a message to the chat every minute
+    """
+    asteroid_info = detector()
+    await message.answer(asteroid_info)
+
+
+async def send_weather_info(bot: Bot) -> None:
+    """
+    Sends weather information
     """
     weather_info = get_weather()
     await bot.send_message(chat_id=CHAT_ID, text=weather_info)
 
+
+@dp.message(Command("weather"))
+async def weather_handler(message: Message) -> None:
+    """
+    Sends weather info in response to the "погода" command
+    """
+    weather_info = get_weather()
+    await message.answer(weather_info)
+
+
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # Запускаем крон-задачи
     crontab('0 9 * * *', func=send_asteroid_info, args=[bot], start=True)
-    crontab('0 9 * * *', func=weather, args=[bot], start=True)
+    crontab('0 9 * * *', func=send_weather_info, args=[bot], start=True)
+
+    # Стартуем диспетчер
     await dp.start_polling(bot)
 
 
